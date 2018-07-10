13bacc590e8bff9b47e480e4af92ca81570f9c77
Hamza Belhaj <hamza.belhaj@kbrwadventure.com>
2018-07-10 15:12:42 +0200

    back end done

--- src/rest.py
+++ src/rest.py
@@ -39,4 +39,58 @@ def get_pending_tx():
 @app.route('/last')
 def get_last():
     return json.dumps(blockchain.last_block().index)
+
+peers = set()
+@app.route('/add_nodes',methods=['POST'])
+def register_new_peers():
+    nodes = request.get_json()
+    if not nodes:
+        return "invalid request", 400
+    for node in nodes:
+        peers.add(node)
+    return "Success", 201
+
+
+
+@app.route('/add_block',methods=['POST'])
+def validate_and_add_block():
+    block_data = request.get_json()
+    block = Block(block_data["index"], block_data["transactions"],block_data["timestamp"], block_data["previous_hash"]])
+    proof = block_data['hash']
+    added = blockchain.add_block(block,proof)
+
+    if not added:
+        return "The block was discarded", 400
+    
+    return "Block added to the chain", 201
+
+def announce_new_block(block):
+    for peer in peers:
+        url = "http://{}/add_block".format(peer)
+        requests.post(url,data = json.dumps(block.__dict__,sort_keys=True))
+
+
+
+def consensus():
+    """ with multiple nodes comes multiple chains we need to define a simple mechanism to
+    choose the correct chain , here we will choose the longer one """
+    global blockchain
+
+    longest_chain = None
+    current_len = len(blockchain)
+
+    for node in peers:
+        response.get('http://{}/chain'.format(node)) 
+        length = response.json()['length']
+        chain = response.json()['chain']
+        if length > current_len and blockchain.check_chain_validity(chain):
+            current_len = length
+            longest_chain = chain
+        if longest_chain:
+            blockchain = longest_chain
+            return True
+        
+        return False
+
+
 app.run(debug=True, port=8080)
